b7baea918a1fd5fd2184bb8b8dd82f20be03b29a
JonAhu <akakech@users.noreply.github.com>
2020-11-03 13:26:53 +0000

    Task 3 Verification and Validation put in place
    I've now added functions to facilatate the validation of the inputs in the Task Form.

--- app.js
+++ app.js
@@ -1,1 +1,88 @@
 // "TaskID":`${taskManager.taskArray.length < 1 ? 1: taskManager.taskArray.length+1}`
+
+// Validation and verification of form inputs Start
+
+// document.querySelector('#addTask').addEventListener('click', 
+// function validateTaskForm(){
+
+//     let isNameValid = document.forms["inputForm"] ["inputName"].value;
+    // if ((isNameValid.length != -1) && (isNameValid > 8)) {
+    //     return true;
+//     }else {
+//         alert ("Please enter a valid name");
+//         return false;
+//     }
+    
+
+// });
+
+// validateTaskForm();
+
+// documentaddTaskButton.addEventListener("click",;
+
+// function validateName() {
+// console.log("Is this button working");
+// return validateName();
+// };
+// validateName();
+
+//  Make form inputs variables.
+
+let inputName = document.getElementById("inputName");
+let inputDesc = document.getElementById("inputDescription");
+let inputAssignedTo = document.getElementById("assignedTo");
+let inputDate = document.getElementById("dueDate");
+let inputStatus = document.getElementById("status");
+
+
+//  Set intial validation booleans, they're false at first, invalid.
+let nameValid = false;
+let descValid = false;
+let assignedToValid = false;
+let dueDateValid = false;
+let statusValid = false;
+let validation = false;
+
+//  When data is entered, run function that checks to see if inputs are valid
+
+function validateTaskForm (inputName, inputDesc, inputAssignedTo, inputDate, inputStatus) {
+    if ((inputName.length != -1) && (inputName.length > 8)) {
+        nameValid = true;
+    } if ((inputDesc.length != -1)&& (inputDesc.length > 20)) {
+        descValid = true;
+    } if ((inputAssignedTo.length != -1)&&(inputAssignedTo.length > 8)) {
+        assignedToValid  = true;
+    } if (inputDate !== "") {
+        dueDateValid = true;
+    } if (inputStatus !== "") {
+        statusValid = true;
+    } if (nameValid && descValid && assignedToValid && dueDateValid && dueDateValid && statusValid === true) {
+        validation = true;
+   
+    }
+    return validation;
+    
+};
+
+// Now the function is made, I need to call the function with the form button.
+document.getElementById("addTask").addEventListener('click', function() {
+    console.log(validateTaskForm(inputName.value, inputDesc.value, inputAssignedTo.value, inputDate.value, inputStatus.value));
+            
+});
+
+// Making a JSON Structure
+let jsonStructure = {
+    "id":"0",
+    "name" : "",
+    "description" : "description",
+    "assignedTo" : "assignedTo",
+    "dueDate" : "dueDate",
+    "status" : "status",
+
+
+
+
+
+
+
+}
